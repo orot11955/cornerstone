8180d3e00e7445395f6df2da39836890772e93d1
orot <orot1195@gmail.com>
2026-08-12 17:01:55 +0900

    chore: type 추가

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,1 +1,18 @@
-export {}
+export type Nullable<T> = T | null
+
+export type Optional<T> = T | undefined
+
+export type SortDirection = 'asc' | 'desc'
+
+export interface PageRequest {
+  page: number
+  size: number
+}
+
+export interface PageResponse<T> {
+  items: T[]
+  page: number
+  size: number
+  total: number
+  totalPages: number
+}
